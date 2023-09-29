b0ddcdf676a9e05376fa2cbc0be4e8694e9dc892
rafalq <kruszewskirafael@gmail.com>
2023-09-29 19:20:58 +0200

    create User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello");
+import { User } from "./models/User";
+
+const user = new User({
+	name: "Johnny",
+	age: 23,
+});
+
+console.log(user.get("name"));
+console.log(user.get("age"));
