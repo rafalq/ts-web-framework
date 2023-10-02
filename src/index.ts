7b86e921c35efa26abfe5147a9760f5d7d5e0ba0
rafalq <kruszewskirafael@gmail.com>
2023-10-02 09:55:50 +0200

    refactor User class in composition type

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { User } from "./models/User";
+
+const user = new User({
+	name: "Composer",
+	age: 0,
+});
+
+user.events.on("change", () => {
+	console.log("change!");
+});
+
+user.events.trigger("change");
