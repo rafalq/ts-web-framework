a80726a434db1cfe5aee42d8875acbf750adfd88
rafalq <kruszewskirafael@gmail.com>
2023-10-04 11:19:01 +0200

    change methods on and trigger for arrow functions in Eventing class

--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -3,16 +3,16 @@ type Callback = () => void;
 export class Eventing {
 	events: { [key: string]: Callback[] } = {};
 
-	on(
+	on = (
 		eventName: string,
 		callback: Callback
-	): void {
+	): void => {
 		const handlers = this.events[eventName] || [];
 		handlers.push(callback);
 		this.events[eventName] = handlers;
-	}
+	};
 
-	trigger(eventName: string): void {
+	trigger = (eventName: string): void => {
 		const handlers = this.events[eventName];
 
 		if (!handlers || handlers.length === 0) {
@@ -22,5 +22,5 @@ export class Eventing {
 		handlers.forEach((callback) => {
 			callback();
 		});
-	}
+	};
 }
