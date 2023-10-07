f97ad09c755377242e48efbac2811e09e75e5a80
rafalq <kruszewskirafael@gmail.com>
2023-10-07 11:33:25 +0200

    add render method to UserForm

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,7 +4,16 @@ export class UserForm {
 		return `
       <div>
         <h1>UserForm</h1>
+        <input />
       </div>
       `;
+	}
+
+	render(): void {
+		const templateEl =
+			document.createElement("template");
+		templateEl.innerHTML = this.template();
+
+		this.parent.append(templateEl.content);
 	}
 }
